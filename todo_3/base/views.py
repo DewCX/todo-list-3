a7d3da6b48ad4f4c4ae5fd97d80ca6ad621fa35d
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-02 13:38:07 +0300

    #Added to how to create item
     #Added connection between with our home page and form page.

--- todo_3/base/views.py
+++ todo_3/base/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+
+
+
+from django.urls import reverse_lazy
 
 
 class TaskList(ListView):
@@ -9,9 +14,14 @@ class TaskList(ListView):
     context_object_name = 'tasks'
 
 
-#Detail
+#Detail To Do Element
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
+#Create To Do Element
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
