1d68812b55d3df89e69c3cb398a0e69985b048e7
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-02 13:13:56 +0300

    #Verify data on database to html page

--- todo_3/base/urls.py
+++ todo_3/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from . views import TaskList
 
 urlpatterns = [
-    path('', views.Home, name='home'),
+    path('', TaskList.as_view(), name='tasks'),
 ]
--- todo_3/base/views.py
+++ todo_3/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
+from django.views.generic.list import ListView
 
-def Home(request):
-    return HttpResponse('Test')
+
+class TaskList(ListView):
+    model = Task
+    
 
