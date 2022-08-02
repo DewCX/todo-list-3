44259dbb34a6e12cc815709db87151f5ee0c7eeb
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-02 16:55:25 +0300

    #Login/Logout,authenticated,restricted

--- todo_3/base/views.py
+++ todo_3/base/views.py
@@ -5,36 +5,49 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
-
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
 
-class TaskList(ListView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+#Login 
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+        
+
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
 
 #Detail To Do Element
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
 #Create To Do Element
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
 
 #Update To Do Element
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
 #Delete To Do Element
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'tasks'
     success_url = reverse_lazy('tasks')
