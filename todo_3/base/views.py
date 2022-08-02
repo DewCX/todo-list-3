b82016ef9038aaf52834ca58bb8f6db887de55b3
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-02 17:09:50 +0300

    #Created user specific data

--- todo_3/base/views.py
+++ todo_3/base/views.py
@@ -21,10 +21,19 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks')
         
 
-
+#Task list
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+
+#User specific data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+            
+        
 
 
 #Detail To Do Element
@@ -36,14 +45,18 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 #Create To Do Element
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 
 #Update To Do Element
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 #Delete To Do Element
