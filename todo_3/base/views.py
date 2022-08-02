f07cdb05dbfa10dd9554e4d4b967db7627c0fc59
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-02 13:22:34 +0300

    #Added link to detail page
    #Customized TaskDetail view

--- todo_3/base/views.py
+++ todo_3/base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import Task
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+#Detail
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
 
